b117bdbd0c62d23174df4f55d38eb1c6e8015f40
Tayná <146398483+Taynssants@users.noreply.github.com>
2024-09-25 09:11:50 -0300

    Update Menu.js

--- Menu.js
+++ Menu.js
@@ -2,18 +2,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Navbar, Nav } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 const Menu = () => {
     return (
-        <Navbar bg="light" expand="lg">
-            <Navbar.Brand as={Link} to="/">Meu App</Navbar.Brand>
-            <Nav className="me-auto">
-                <Nav.Link as={Link} to="/produtos">Produtos</Nav.Link>
-                <Nav.Link as={Link} to="/cria-produto">Criar Produto</Nav.Link>
-                <Nav.Link as={Link} to="/selecionados">Selecionados</Nav.Link>
-            </Nav>
-        </Navbar>
-    );
-};
+      <Container>
+    <NavLink to = {'criaProduto'}> Cria Produto</NavLink>
+    <NavLink to = {'produtos'}> Produtos</NavLink>
+    <NavLink to= {'app'}> App</NavLink>
+        </Container>
 
 export default Menu;
