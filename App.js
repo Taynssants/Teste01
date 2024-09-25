1847cde9ce8d510e11f60df44ca65e98606eed49
Tayná <146398483+Taynssants@users.noreply.github.com>
2024-09-25 09:03:06 -0300

    Update App.js

--- App.js
+++ App.js
@@ -12,17 +12,11 @@ const App = () => {
         <Router>
             <Menu /> {/* Incluindo o Menu */}
             <div className="container mt-3"> {/* Container para melhor espaçamento */}
-                <Switch>
                     <Route path="/produtos" component={Produtos} />
                     <Route path="/cria-produto" component={CriaProduto} />
                     <Route path="/selecionados" component={Selecionados} />
                     <Route path="/" exact>
-                        <h1>Bem-vindo ao Meu App!</h1> {/* Rota padrão */}
                     </Route>
-                    <Route path="*">
-                        <h2>Página não encontrada!</h2> {/* Rota para páginas não definidas */}
-                    </Route>
-                </Switch>
             </div>
         </Router>
     );
